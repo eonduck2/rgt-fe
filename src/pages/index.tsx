d2a1cc07eba315dd5ff396b319f3eb11f6a8d4b5
eonduck2 <whdtnxd@gmail.com>
2024-10-29 21:48:01 +0900

    :package: 주문 폼 메인 페이지에 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,11 @@
-export default () => {
-  return <div>ㅎㅇ</div>;
-};
+// pages/index.tsx
+import OrderForm from "@/component/form/orderForm";
+
+export default function Home() {
+  return (
+    <div className="justify-center flex items-center w-screen h-screen">
+      <OrderForm />
+    </div>
+  );
+}
 
