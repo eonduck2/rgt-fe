ea8936afc12ecf819f07adc0c048847788124456
eonduck2 <whdtnxd@gmail.com>
2024-10-29 21:47:05 +0900

    :package: 주문 폼 넓이 조정

--- src/component/form/orderForm.tsx
+++ src/component/form/orderForm.tsx
@@ -15,7 +15,7 @@ export default () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-lg"
+      className="w-1/3 mx-auto p-6 bg-white rounded-lg shadow-lg"
     >
       <h2 className="text-2xl font-bold text-gray-800 mb-6 text-center">
         주문하기
