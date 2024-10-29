24b8c8951b08c5ab525a0c44c2cd02b3221b2a41
eonduck2 <whdtnxd@gmail.com>
2024-10-29 17:37:52 +0900

    :package: 주문 버튼 컴포넌트 구현

---
+++ src/component/button/orderButton.tsx
@@ -0,0 +1,10 @@
+export default () => {
+  return (
+    <button
+      type="submit"
+      className="w-full bg-blue-600 text-white py-3 px-4 rounded-lg font-semibold hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors"
+    >
+      주문하기
+    </button>
+  );
+};
