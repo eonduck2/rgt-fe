75ee68a8826b80b9c65fa1d280bb01c265df3b06
eonduck2 <whdtnxd@gmail.com>
2024-10-29 21:47:36 +0900

    :bulb: 음식 주문 주석 수정

--- src/component/input/order/orderFoodInput.tsx
+++ src/component/input/order/orderFoodInput.tsx
@@ -5,8 +5,8 @@ import React from "react";
 /**
  * @eonduck2 24.10.29
  * @description 사용자가 수량을 입력할 수 있는 입력 필드 컴포넌트입니다.
- * @param { string } props.quantity - 주문할 수량을 나타내는 값입니다.
- * @param { (value: number) => void } props.stateSetter - 수량 상태를 업데이트하는 함수입니다.
+ * @param { string } props.foodName - 주문 음식의 이름을 나타내는 값입니다.
+ * @param { (value: string) => void } props.stateSetter - 수량 상태를 업데이트하는 함수입니다.
  * @param { string } props.labelContent - 입력 필드의 레이블 내용을 나타내는 문자열입니다.
  * @returns { JSX.Element } 입력 필드와 레이블을 포함하는 JSX 요소입니다.
  */
