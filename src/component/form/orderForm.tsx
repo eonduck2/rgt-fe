5e080ffd6088098fdba6512c52315d4f2a07a050
eonduck2 <whdtnxd@gmail.com>
2024-10-30 17:13:30 +0900

    :package: 주문 요청 부분 추가

--- src/component/form/orderForm.tsx
+++ src/component/form/orderForm.tsx
@@ -2,14 +2,38 @@ import { useState } from "react";
 import OrderButton from "../button/orderButton";
 import OrderFoodInput from "../input/order/orderFoodInput";
 import OrderQuantityInput from "../input/order/orderQuantityInput";
+import fetcher from "@/module/fetching/fetcher";
+import thrower from "@/module/throw/thrower";
+import mime from "mime";
+import HttpMethod from "@/static/shared/http/httpMethod.static";
+import urlJoiner from "@/module/url/urlJoiner";
 
 export default () => {
   const [foodName, setFoodName] = useState("");
   const [quantity, setQuantity] = useState(1);
+  const BE_URL = process.env.NEXT_PUBLIC_BE_URL as string;
+  const EP_ORDER = process.env.NEXT_PUBLIC_EP_ORDER as string;
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log({ foodName, quantity });
+
+    const orderData = { foodName, quantity };
+
+    try {
+      const response = await fetcher(urlJoiner(BE_URL, EP_ORDER), {
+        method: HttpMethod.POST,
+        headers: {
+          "Content-Type": mime.getType("json") as string,
+        },
+        body: JSON.stringify(orderData),
+      });
+
+      const data = await response.json();
+
+      console.log(data);
+    } catch (error) {
+      thrower(error as Error);
+    }
   };
 
   return (
