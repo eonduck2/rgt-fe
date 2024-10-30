0c0367517927a2218e5ab55b0e54c42423c93f9f
eonduck2 <whdtnxd@gmail.com>
2024-10-30 18:21:52 +0900

    :white_check_mark: urlJoiner 테스트 그룹 설명 수정

--- __test__/module/url/urlJoiner.test.ts
+++ __test__/module/url/urlJoiner.test.ts
@@ -1,6 +1,6 @@
 import urlJoiner from "@/module/url/urlJoiner";
 
-describe("urlJoiner", () => {
+describe("urlJoiner 모듈에 대한 테스트 케이스 집합입니다.", () => {
   it("두 개의 간단한 경로를 조합합니다.", () => {
     const result = urlJoiner("https://test.com", "testPath");
     expect(result).toBe("https://test.com/testPath");
@@ -31,7 +31,7 @@ describe("urlJoiner", () => {
       "https://test.com/",
       "testPath/testPath2/testPath3/"
     );
-    expect(result).toBe("https://test.com/testPath/testPath2/testPath3/");
+    expect(result).toBe("https://test.com/testPath/testPath2/testPath3");
   });
 
   it("빈 문자열을 포함하여 경로를 조합합니다.", () => {
