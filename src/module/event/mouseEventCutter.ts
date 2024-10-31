615a4ae1935d2dcff3acb2c4dfdc1495c1fbcaf8
eonduck2 <whdtnxd@gmail.com>
2024-10-31 15:31:26 +0900

    :sparkles: 마우스 관련 이벤트 전파 방지 모듈 생성

--- src/module/event/mouseEventCutter.ts
+++ src/module/event/mouseEventCutter.ts
@@ -3,5 +3,7 @@
  * @description 모ㅌ달 내부 클릭 시 이벤트 전파(버블링) 방지
  * @param { React.MouseEvent } e 마우스(클릭) 이벤트 객체
  */
-export default (e: React.MouseEvent) => e.stopPropagation();
+export default (e: React.MouseEvent) => {
+  e.stopPropagation();
+};
 
