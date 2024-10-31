d5982ec38ea1adefcf70ec754fc104b1179b00d7
eonduck2 <whdtnxd@gmail.com>
2024-10-31 15:31:09 +0900

    :wrench: 메시지 및 주문 모달 프로퍼티 형식 정의

---
+++ src/types/component/modal/orderStateModalProps.type.ts
@@ -0,0 +1,9 @@
+import type TMessage from "@/types/shared/message.type";
+
+type TOrderStateModalProps = {
+  message: TMessage;
+  onClose: () => void;
+};
+
+export default TOrderStateModalProps;
+
---
+++ src/types/shared/message.type.ts
@@ -0,0 +1,11 @@
+/**
+ * @eonduck2 24.10.31
+ * @description 메시지에 대한 범용 형식 정의입니다.
+ *
+ * @typedef { string }
+ */
+
+type TMessage = string;
+
+export default TMessage;
+
