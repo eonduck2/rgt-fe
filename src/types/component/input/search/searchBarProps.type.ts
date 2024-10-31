f873b95e86f85dca9222dad6b22a31ecb4830c54
eonduck2 <whdtnxd@gmail.com>
2024-10-31 18:36:24 +0900

    :bulb: 검색어 props 형식 파일 주석 수정

--- src/types/component/input/search/searchBarProps.type.ts
+++ src/types/component/input/search/searchBarProps.type.ts
@@ -2,11 +2,11 @@ import type TValue from "@/types/shared/value.type";
 
 /**
  * @eonduck2 24.10.31
- * @description 수량 인풋의 property들을 정의한 형식입니다.
+ * @description 검색어 인풋창 속성에 대한 형식
  *
  * @typedef { Object } TSearchBarProps
- * @property { string } value 주문할 수량을 나타내는 값입니다.
- * @property { (value: string) => void } setter 수량 상태를 업데이트하는 함수입니다.
+ * @property { string } value 검색어
+ * @property { (value: string) => void } setter 검색어 세터
  */
 type TSearchBarProps = {
   value: TValue;
